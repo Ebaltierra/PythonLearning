68636ccfd3cd213924a14e161af7774f3313c06e
Ebaltierra <ezzy123we@gmail.com>
2023-05-10 09:05:10 -0500

    Excersize

---
+++ Day 1-3 (input function).py
@@ -0,0 +1,5 @@
+# Input() gest the user input through the console
+# Print() takes the input and concantenates it with the print
+# Input("What day is it?")
+
+print("Hello " + input("What is your name?"))
