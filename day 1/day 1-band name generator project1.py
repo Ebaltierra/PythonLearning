3ffcff16b07c2cbd681374d683bdfc563c0854d0
Ebaltierra <ezzy123we@gmail.com>
2023-05-10 11:27:37 -0500

    Finished day 1

--- day 1/day 1-band name generator project1.py
+++ day 1/day 1-band name generator project1.py
@@ -1,5 +1,10 @@
-Animal = input("What is your favorite animal?")
-color= input("What is your favorite color?")
- 
-print("Your band name is the " + 
+print("Hello, let's generate your band name!" 
+      + "\n"
+      + "\nWhat is your favorite animal?")
+Animal = input("\n")
+print("\nWow that's cool!"
+      + "\n" 
+      + "\nWhat is your favorite color?")
+color= input("\n")
+print("\nWell then! Your band name is the " + 
       color + " " + Animal + "'s")
