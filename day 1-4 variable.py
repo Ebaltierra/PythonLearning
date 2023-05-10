629d4c03346256ffcb697047e7becc986d919dd0
Ebaltierra <ezzy123we@gmail.com>
2023-05-10 10:02:46 -0500

    New lessons

---
+++ day 1-4 variable.py
@@ -0,0 +1,5 @@
+name= len(input("What is your name?"))
+
+print("Your name is "
+      + str(name) 
+      + " characters long")
---
+++ day 1-band name generator project1.py
@@ -0,0 +1,5 @@
+Animal = input("What is your favorite animal?")
+color= input("What is your favorite color?")
+ 
+print("Your band name is the " + 
+      color + " " + Animal + "'s")
