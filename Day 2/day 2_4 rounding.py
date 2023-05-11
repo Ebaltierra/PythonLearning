7f8a97b8d8f50af008128742f7f7a78ffc39feda
ebaltierra <ezzy123we@gmail.com>
2023-05-11 01:25:06 -0500

    Day 2 commit

---
+++ Day 2/day 2_4 rounding.py
@@ -0,0 +1,13 @@
+num_1= int(input("Pick a number:  "))
+num_2= int(input("Pick a second number:  "))
+num_3= int(input("How many numbers do I round to?:  "))
+print(round(num_1/num_2, num_3))
+
+score = 0 
+height = 1.6
+isWinning= True
+
+print(f"Your score is {score}"
+    + f"\nYour height is {height}" 
+      + f"\nYour winning status is {isWinning}")
+
