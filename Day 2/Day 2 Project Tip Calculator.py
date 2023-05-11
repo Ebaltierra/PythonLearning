ba54aee534f388c8c409be5886c45434c82815e1
Ebaltierra <ezzy123we@gmail.com>
2023-05-11 00:32:16 -0500

    Day 2

---
+++ Day 2/Day 2 Project Tip Calculator.py
@@ -0,0 +1,14 @@
+print("Welcome to the tip calculator.")
+print("\nWhat was the total bill?")
+bill=int(input("\n"))
+print("\nHow many people are splitting the bill?")
+people=int(input("\n"))
+print("\nWhat tip would you like to provide?"
+      +"\n10, 12, 15, 18, or 20?")
+tip=int(input("\n"))
+bill_2= (bill/people)
+tip_2= (1 + tip/100)
+total= (bill_2*tip_2*1.0825)
+print("\nEach person should pay $" + str(total))
+
+
