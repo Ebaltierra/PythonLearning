7293145e79ac4b8caeea0ecacd49717806416c1d
Ebaltierra <ezzy123we@gmail.com>
2023-05-15 11:45:50 -0500

    null

---
+++ Day 3/Day1_6.py
@@ -0,0 +1,19 @@
+print("\nWhat is your height in cm?")
+height=int(input("\n: "))
+print("\nHow old are you?")
+age=int(input("\n: "))
+if height>=120: 
+    if age<12:
+        ticket_price=5
+    elif age<=18:
+        ticket_price=7
+    elif age>=45 and age<=55:
+        ticket_price=0
+    else: 
+        ticket_price=12
+    wants_photo= input("\nDo you want a photo taken? Y or N")
+    if wants_photo==("Y"):
+        ticket_price= ticket_price+3
+    print(f"Your ticket will be {ticket_price} dollars")
+else:
+    print("You are too short to ride.")
