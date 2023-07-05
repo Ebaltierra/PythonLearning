4ad72a6fdd95131843071396728d74c52962902d
ebaltierra <ezzy123we@gmail.com>
2023-07-04 22:46:29 -0500

    na

---
+++ Day 4/day1_pi.py
@@ -0,0 +1,23 @@
+import random 
+
+# random_integer = random.randint(1,10)
+# print(random_integer)
+
+# random_float = round(random.random()*5, 5)
+# print(random_float)
+
+# love_score=random.randint(1,100)
+# print (f"Your love score is {love_score}")
+
+# coin_status = random.randint(1,2)
+# if coin_status==1:
+#     coin_status="Heads"
+# else:
+#     coin_status="Tails"
+    
+# print(coin_status)
+num=1
+states_of_america=['Delaware', 'Pennsylvania']
+print(states_of_america[0])
+states_of_america.append("Texas")
+print(states_of_america[-1])
